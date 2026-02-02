e1224570a863d32f736b441319d0cabaef42f63d
Davyd Holovii <mirage.present@gmail.com>
2026-02-02 17:28:21 -0500

    initial idea

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/miragepresent/go-sse-delivery
+
+go 1.25.5
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Go-SSE-Delivery is a tool that creates a SSE endpoint for events delivery from backend")
+}
